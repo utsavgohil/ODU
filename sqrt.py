bb956e3bd81c47fa1b8a777ab31fb3806fd3936f
utsavgohil <gohilutsav@gmail.com>
2016-07-13 23:21:30 -0400

    Add files via upload

---
+++ sqrt.py
@@ -0,0 +1,13 @@
+import math
+x1=input("Enter the cordinate of x")
+y1=input ("Enter the coordinate of y")
+z1=input ("Enter the coordinate of z")
+x2=input ("Enter the coordinate of x")
+y2=input ("Enter the coordinate of y")
+z2=input ("Enter the coordinate of z")
+x=x1-x2
+y=y1-y2
+z=z1-z2
+total=x+y+z
+num=(math.sqrt(total))
+print num
